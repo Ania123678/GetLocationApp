caaadb6eac5fc3716399cbde7ef26e3a80e11df2
Ania123678 <anna.kus084@gmail.com>
2022-06-14 20:27:26 +0200

    current location update system

--- app/src/main/java/android/example/com/getlocationapp/CurrentLocationInfo.java
+++ app/src/main/java/android/example/com/getlocationapp/CurrentLocationInfo.java
@@ -1,13 +1,151 @@
 package android.example.com.getlocationapp;
 
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class CurrentLocationInfo {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CurrentLocationInfo extends Fragment implements LocationListener {
+
+    private String mParam1;
+    private String mParam2;
+
+    public CurrentLocationInfo() {
+        // Required empty public constructor
+    }
+
+    private View myView = null;
+
+    public static final int PERMISSION_FINE_LOCATION = 1;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    LocationManager lm;
+
+    Context context;
+    DataBase myDB;
+
+    private TextView textLongitude, textLatitude, textAltitude, textTime, textSpeed, textAccuracy;
+
+    Button buttonSave;
+    boolean updateOn = false;
+
+    ArrayList<Double> longitude_array = new ArrayList<>();
+    ArrayList<Double> latitude_array = new ArrayList<>();
+    int count = 0;
+
+    String longitude;
+    String latitude;
+    String altitude;
+    String time;
+    String speed;
+    String accuracy;
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.location_info, container, false);
 
 
 
+
+
+        //----------------------- przycisk do zapisywania danych w bazie danych  ----------------------- //
+//        Button buttonSave = myView.findViewById(R.id.buttonSave);
+//        buttonSave.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                DataBase myDB = new DataBase(CurrentLocationInfo.this);
+//                myDB.addInfo(longitude, latitude, altitude, time, speed, accuracy);
+//            }
+//        });
+
+
+        LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1, this);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        return myView;
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        if (null != myView) {
+            TextView textLongitude = myView.findViewById(R.id.longitude);
+            TextView textLatitude = myView.findViewById(R.id.latitude);
+            TextView textAltitude = myView.findViewById(R.id.altitude);
+            TextView textTime = myView.findViewById(R.id.time);
+            TextView textSpeed = myView.findViewById(R.id.speed);
+            TextView textAccuracy = myView.findViewById(R.id.accuracy);
+
+
+            String longitude = String.valueOf(location.getLongitude());
+            String latitude = String.valueOf(location.getLatitude());
+            String altitude = String.valueOf(location.getAltitude());
+            String time = String.valueOf(location.getTime());
+            String speed = String.valueOf(location.getSpeed());
+            String accuracy = String.valueOf(location.getAccuracy());
+
+
+            textLongitude.setText(longitude);
+            textLatitude.setText(latitude);
+            textAltitude.setText(altitude);
+            textTime.setText(time);
+            textSpeed.setText(speed);
+            textAccuracy.setText(accuracy);
+        }
+    }
+
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
 }
--- app/src/main/java/android/example/com/getlocationapp/MainActivity.java
+++ app/src/main/java/android/example/com/getlocationapp/MainActivity.java
@@ -7,41 +7,39 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.location.LocationListener;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int PERMISSION_FINE_LOCATION = 1;
-    public static final int DEFAULT_UPDATE_VALUE = 30;
-    public static final int FAST_UPDATE_VALUE = 50;
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private LocationRequest locationRequest;
-
-    DataBase myDB;
-
-    private TextView textLongitude, textLatitude, textAltitude, textTime, textSpeed, textAccuracy;
-    String s1 = "Altitude information is not available";
-    String s2 = "Speed information is not available";
 
     Button buttonPermission;
     Button buttonShow;
-    Button buttonSave;
     Button buttonShowList;
 
-    boolean updateOn = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_page);
 
 
-        Button buttonPermission  = findViewById(R.id.buttonPermission);
+        //  ----------------------- przycisk do uprawnień   ----------------------- //
+        Button buttonPermission = findViewById(R.id.buttonPermission);
         buttonPermission.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,89 +62,39 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        //  ---------------------------------------------- //
 
-        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        //-----------------------------dane-----------------------------//
-        String longitude = String.valueOf(location.getLongitude());
-        String latitude = String.valueOf(location.getLatitude());
-        String altitude = String.valueOf(location.getAltitude());
-        String time = String.valueOf(location.getTime());
-        String speed = String.valueOf(location.getSpeed());
-        String accuracy = String.valueOf(location.getAccuracy());
+        //  ----------------------- przycisk do wyswietlania danych lokalizacji   ----------------------- //
+        Button buttonShow = findViewById(R.id.buttonShow);
+        buttonShow.setOnClickListener(this::onClick);
+        //  ---------------------------------------------- //
 
-        //-------------------------------------------------------------//
-
-        Button buttonShow  = findViewById(R.id.buttonShow);
-        buttonShow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //dlugosc
-                TextView textLongitude = (TextView) findViewById(R.id.longitude);
-                textLongitude.setText(longitude);
-
-                //szerokosc
-                TextView textLatitude = (TextView) findViewById(R.id.latitude);
-                textLatitude.setText(latitude);
-
-                //wysokosc
-                TextView textAltitude = (TextView) findViewById(R.id.altitude);
-                if(location.hasAltitude()){
-                    textAltitude.setText(altitude);
-                }
-                else{
-                    textAltitude.setText(s1);
-                }
-
-                //czas
-                TextView textTime = (TextView) findViewById(R.id.time);
-                textTime.setText(time);
-
-                //predkosc
-                TextView textSpeed = (TextView) findViewById(R.id.speed);
-                if(location.hasSpeed()){
-                    textAltitude.setText(speed);
-                }
-                else{
-                    textAltitude.setText(s2);
-                }
-                //dokladnosc
-                TextView textAccuracy = (TextView) findViewById(R.id.accuracy);
-                textAccuracy.setText(accuracy);
-
-            }
-        });
-
-        Button buttonSave = findViewById(R.id.buttonSave);
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DataBase myDB = new DataBase(MainActivity.this);
-                myDB.addInfo(longitude, latitude, altitude, time, speed, accuracy);
-            }
-        });
-
+        //  ----------------------- przycisk do przekierowania do listy danych  ----------------------- //
         Button buttonShowList = findViewById(R.id.buttonShowList);
         buttonShowList.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                openListInfo();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListInfo.class);
+                startActivity(intent);
             }
         });
+        //  ---------------------------------------------- //
     }
 
-    public void openListInfo(){
-        Intent intent = new Intent(this, ListInfo.class);
-        startActivity(intent);
+    public void onClick(View w){
+        if(w.getId()==R.id.buttonShow){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,new CurrentLocationInfo()).commit();
+        }
     }
+
 
     private void requestLocationPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)) {
             new AlertDialog.Builder(this)
                     .setTitle("Permission needed")
-                    .setMessage("This permission is needed for WIFI access")
+                    .setMessage("This permission is needed for Location access")
                     .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
@@ -168,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_FINE_LOCATION ) {
+        if (requestCode == PERMISSION_FINE_LOCATION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
             } else {
@@ -176,73 +125,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//
-//        switch(requestCode){
-//            case PERMISSION_FINE_LOCATION:
-//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-//                    updateGPS();
-//
-//                }
-//                else{
-//                    Toast.makeText(MainActivity.this, "This permission is needed for location access", Toast.LENGTH_SHORT).show();
-//                    finish();
-//                }
-//                break;
-//        }
-//    }
-
-//    private void updateGPS(){
-//
-//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-//
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-//            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>(){
-//                @Override
-//                public void onSuccess(Location location){
-//                    Toast.makeText(MainActivity.this, "Permission granted", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//        }
-//
-//        else{
-//            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-//                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
-//            }
-//        }
-//    }
-
-//    private void updateLocationValues(Location location){
-//
-//        //dlugosc
-//        textLongitude.setText(String.valueOf(location.getLongitude()));
-//
-//        //szerokosc
-//        textLatitude.setText(String.valueOf(location.getLatitude()));
-//
-//        //wysokosc
-//        if(location.hasAltitude()){
-//            textAltitude.setText(String.valueOf(location.getAltitude()));
-//        }
-//        else{
-//            textAltitude.setText(s1);
-//        }
-//
-//        //czas
-//        textTime.setText(String.valueOf(location.getTime()));
-//
-//        //predkosc
-//        if(location.hasSpeed()){
-//            textAltitude.setText(String.valueOf(location.getSpeed()));
-//        }
-//        else{
-//            textAltitude.setText(s2);
-//        }
-//        //dokladnosc
-//        textAccuracy.setText(String.valueOf(location.getAccuracy()));
-//    }
 }
